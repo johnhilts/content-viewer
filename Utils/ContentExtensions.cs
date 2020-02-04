91b502474b696bfc00f41fc150d0df44272637e5
John Hilts <johnhilts@live.com>
2020-02-04 19:43:19 +0000

    use Task.WhenAll to get back to loopless

--- Utils/ContentExtensions.cs
+++ Utils/ContentExtensions.cs
@@ -21,40 +21,40 @@ namespace dotnet.Utils
             return folders.Select(folder => new ContentModel {Name = Path.GetFileName(folder), ContentType = ContentType.Folder, });
         }
 
-        public static async Task<List<ContentModel>> GetFiles(this string currentFolder)
+        public static async Task<IEnumerable<ContentModel>> GetFiles(this string currentFolder)
         {
-            var files = Directory.GetFiles(currentFolder)
-                .Where(file => !Path.GetFileName(file).StartsWith(@"."));
-            var contentFiles = new List<ContentModel>();
-            foreach(var file in files)
-            {
-                var geoCoordinateText = string.Empty;
-                try
-                {
-                    var coordinateModel = (new ExInfoHelper()).ExtractGeoCoordinates(file);
-                    try
+            return 
+                (await Task.WhenAll(
+                    Directory.GetFiles(currentFolder)
+                    .Where(file => !Path.GetFileName(file).StartsWith(@"."))
+                    .Select(async (file) =>  
                     {
-                        geoCoordinateText = (await (new GeocodeHelper()).ReverseGeocode(coordinateModel));
-                    }
-                    catch (Exception exception)
-                    {
-                        Console.WriteLine($"reverse geo-coding error: {exception.ToString()}");
-                    }
-                }
-                catch (Exception exception)
-                {
-                    Console.WriteLine($"file io error: {exception.ToString()}");
-                }
-                contentFiles.Add(
-                    new ContentModel 
-                    {
-                        Name = Path.GetFileName(file), 
-                        ContentType = ContentType.File, 
-                        Created = File.GetCreationTime(file).ToShortDateString(),
-                        GeoCoordinateText = geoCoordinateText,
-                    });
-            }
-            return contentFiles;
+                        var geoCoordinateText = string.Empty;
+                        try
+                        {
+                            var coordinateModel = (new ExInfoHelper()).ExtractGeoCoordinates(file);
+                            try
+                            {
+                                geoCoordinateText = (await (new GeocodeHelper()).ReverseGeocode(coordinateModel));
+                            }
+                            catch (Exception exception)
+                            {
+                                Console.WriteLine($"reverse geo-coding error: {exception.ToString()}");
+                            }
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine($"file io error: {exception.ToString()}");
+                        }
+                        return new ContentModel 
+                            {
+                                Name = Path.GetFileName(file), 
+                                ContentType = ContentType.File, 
+                                Created = File.GetCreationTime(file).ToShortDateString(),
+                                GeoCoordinateText = geoCoordinateText,
+                            };
+                    })
+                ));
         }
 
     }
